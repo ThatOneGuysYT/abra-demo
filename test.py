21a457878e6478c4fb37e79cc4eedd399adc85db
RotlevB <ben@rotlevy.com>
2024-01-29 14:51:55 -0800

    push

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import pytest
 def test_1():
     url = "http://flask_server:5000/"
     resp = requests.get(url).text
-    assert resp == 'test_passed'
+    assert resp == 'test_passed1'
 
 test_1()
