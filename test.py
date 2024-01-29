48d4adb188494df298b1665639798f1227c56c02
RotlevB <ben@rotlevy.com>
2024-01-29 13:49:22 -0800

    push

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import requests
+import pytest
 
 def test1():
     url = "https://reqres.in/api/users/2"
     resp = requests.get(url).json()["data"]['email']
     assert resp == 'janet.weaver@reqres.in'
-
-test1()
